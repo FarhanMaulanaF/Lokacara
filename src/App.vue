a6322bf1605980ed3ea0de329b461d729852aba0
Farhan Maulana Firmansyah <79140705+FarhanMaulanaF@users.noreply.github.com>
2021-09-17 14:46:52 +0700

    dashboard on progress

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,58 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<!-- in wrapper -->
+<div class="wrapper">
+  <!-- in navigation-drwaer -->
+  <v-navigation-drawer
+   color="#5D5F61"
+   height="100vh"
+   width="320px"
+   dark> 
+   <!-- in logo-lokacara -->
+    <ul>
+      <li>
+        <img 
+        src="@/components/logo-lokacara.svg"
+        height="32px"
+        width="165.94px">
+      </li>
+    </ul>
+    <!-- out logo lokacara -->
+
+    <v-divider light></v-divider>
+
+    <!-- in list-navigation -->
+    <ul class="list navigation">
+      <li>
+        <a href="#" class="dashboard"><img src="@/components/logo-dashboard.svg"><span>Dashboard</span></a>
+      </li>
+      <li>
+        <a href="#" class="event-list"><img src="@/components/logo-eventList.svg"><span>Event List</span></a>
+      </li>
+    </ul>
+    <!-- out list-navigation -->
+
+    <v-divider></v-divider>
+  </v-navigation-drawer>
+  <!-- out navigation-drawer -->
+</div>
+<!-- out wrapper -->
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld';
 
 export default {
   name: 'App',
+
   components: {
-    HelloWorld
-  }
-}
+    // HelloWorld,
+  },
+
+  data: () => ({
+    //
+  }),
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
